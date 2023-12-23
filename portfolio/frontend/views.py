e9825040c1afce0ca5ae8ae5edf9633d272166d4
AshiqMohammed-M <ashiqmohammed0055@gmail.com>
2023-12-23 18:23:56 +0530

    portfolio created

---
+++ portfolio/frontend/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse(
+        '''<h1 style="font-family:fantasy;
+        padding: 355px 0;
+        text-align: center;">server started</h1>'''
+    )
+
+def portfolio(request):
+    return render(request,'frontend/index.html')
+
+
+
